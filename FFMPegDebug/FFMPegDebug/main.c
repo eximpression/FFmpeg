5ad3b92c7e36ba97518eb2601d02ea39eab7da85
cleverzhang <cleverzhang@tencent.com>
2019-11-03 16:04:00 +0800

    增加xcode调试

---
+++ FFMPegDebug/FFMPegDebug/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FFMPegDebug
+//
+//  Created by ZhangChao on 2019/11/3.
+//  Copyright © 2019 ZhangChao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
